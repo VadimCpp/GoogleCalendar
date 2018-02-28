a41385de3bacfb15e509508273d7c831c4744eb2
Vadim Cpp <vadim.cpp@gmail.com>
2018-02-28 15:02:50 +0200

    Highlight event's days

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -1138,16 +1138,138 @@ var CalendarView = function () {
        */
       var that = this;
 
+      /**
+       * @type {!Date}
+       */
+      var today = new Date();
+
       that._data = data;
 
       console.log('TEST');
       console.log(data);
 
+      var events = that.getEvents();
+
+      console.log('events', events);
       /**
        * Init and render Kendo UI calendar
        */
+      $('#calendar-view').kendoCalendar({
+        value: today,
+        change: function change() {
+          that.onDateChanged(this.value());
+        },
+        dates: events,
+        month: {
+          content: '<div class="' + '# if ($.inArray(moment(data.date).format("YYYY-MM-DD"), data.dates) != -1) { #' + 'calendar-events' + '# } else { #' + 'calendar-no-events' + '# } #' + '">#= data.value # </div>'
 
-      $('#calendar-view').kendoCalendar();
+        }
+      });
+    }
+
+    /**
+     * @param {Date} value
+     * @public
+     */
+
+  }, {
+    key: 'onDateChanged',
+    value: function onDateChanged(value) {
+      console.log(value);
+      console.log(this.getEventsNo(value));
+      // TODO: choose value
+    }
+
+    /**
+     * Return array of dates when event happens.
+     * If no events empty array is returned.
+     *
+     * @param {Date} date
+     * @return {Array}
+     * @public
+     */
+
+  }, {
+    key: 'getEvents',
+    value: function getEvents() {
+      /**
+       * @type {Array}
+       */
+      var events = [];
+
+      if (this._data && this._data.items && this._data.items.length) {
+        /**
+         * @type {number}
+         */
+        var length = this._data.items.length;
+
+        for (var i = 0; i < length; i++) {
+          /**
+           * @type {number}
+           */
+          var item = this._data.items[i];
+
+          /**
+           * @type {string}
+           */
+          var started = moment(item.start.dateTime).format("YYYY-MM-DD");
+
+          if (events.indexOf(started) == -1) {
+            events.push(started);
+          }
+        }
+      }
+
+      return events;
+    }
+
+    /**
+     * Return number of events which happens on the specified date.
+     * If no events found returns 0.
+     *
+     * @param {Date} date
+     * @return {number}
+     * @public
+     */
+
+  }, {
+    key: 'getEventsNo',
+    value: function getEventsNo(date) {
+
+      /**
+       * @type {string}
+       */
+      var momentDate = moment(date).format("YYYY-MM-DD");
+
+      /**
+       * @type {number}
+       */
+      var retVal = 0;
+
+      if (this._data && this._data.items && this._data.items.length) {
+        /**
+         * @type {number}
+         */
+        var length = this._data.items.length;
+
+        for (var i = 0; i < length; i++) {
+          /**
+           * @type {number}
+           */
+          var item = this._data.items[i];
+
+          /**
+           * @type {string}
+           */
+          var started = moment(item.start.dateTime).format("YYYY-MM-DD");
+
+          if (started == momentDate) {
+            retVal++;
+          }
+        }
+      }
+
+      return retVal;
     }
   }]);
 
--- src/view/calendarview.js
+++ src/view/calendarview.js
@@ -20,9 +20,7 @@ export default class CalendarView {
          * @private
          */
         this._data = null;
-
     }
-
 
     /**
      * @param {!Object} data
@@ -34,16 +32,138 @@ export default class CalendarView {
          */
         let that = this;
 
+        /**
+         * @type {!Date}
+         */
+        const today = new Date();
+
         that._data = data;
 
         console.log('TEST');
         console.log(data);
 
+        let events = that.getEvents();
+
+        console.log('events', events)
         /**
          * Init and render Kendo UI calendar
          */
-
-        $('#calendar-view').kendoCalendar();
+        $('#calendar-view').kendoCalendar({
+            value: today,
+            change: function() {
+                that.onDateChanged(this.value());
+            },
+            dates: events,
+            month: {
+                content: 
+                    '<div class="' +
+                        '# if ($.inArray(moment(data.date).format("YYYY-MM-DD"), data.dates) != -1) { #' +
+                            'calendar-events' + 
+                        '# } else { #' +
+                            'calendar-no-events' + 
+                        '# } #' +
+                    '">#= data.value # </div>'
+                    
+            }
+        });
     }
 
+
+    /**
+     * @param {Date} value
+     * @public
+     */
+    onDateChanged(value) {
+        console.log(value);
+        console.log(this.getEventsNo(value));
+        // TODO: choose value
+    }
+
+
+    /**
+     * Return array of dates when event happens.
+     * If no events empty array is returned.
+     *
+     * @param {Date} date
+     * @return {Array}
+     * @public
+     */
+    getEvents() {
+        /**
+         * @type {Array}
+         */
+        let events = [];
+
+        if (this._data && this._data.items && this._data.items.length) {
+            /**
+             * @type {number}
+             */
+            let length = this._data.items.length;
+
+            for (let i = 0; i < length; i++) {
+                /**
+                 * @type {number}
+                 */
+                let item = this._data.items[i];
+
+                /**
+                 * @type {string}
+                 */
+                let started = moment(item.start.dateTime).format("YYYY-MM-DD");
+
+                if (events.indexOf(started) == -1) {
+                    events.push(started);
+                }
+            }
+        }
+
+        return events;
+    }
+
+
+    /**
+     * Return number of events which happens on the specified date.
+     * If no events found returns 0.
+     *
+     * @param {Date} date
+     * @return {number}
+     * @public
+     */
+    getEventsNo(date) {
+
+        /**
+         * @type {string}
+         */
+        let momentDate = moment(date).format("YYYY-MM-DD");
+
+        /**
+         * @type {number}
+         */
+        var retVal = 0;
+
+        if (this._data && this._data.items && this._data.items.length) {
+            /**
+             * @type {number}
+             */
+            let length = this._data.items.length;
+
+            for (let i = 0; i < length; i++) {
+                /**
+                 * @type {number}
+                 */
+                let item = this._data.items[i];
+
+                /**
+                 * @type {string}
+                 */
+                let started = moment(item.start.dateTime).format("YYYY-MM-DD");
+
+                if (started == momentDate) {
+                    retVal++;
+                }
+            }
+        }
+
+        return retVal;
+    }
 };
